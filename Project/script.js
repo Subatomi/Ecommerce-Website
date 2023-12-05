ac9dc20d900c8f1e76c27c3e72097217b1d74242
Subatomi <laderatheo@gmail.com>
2023-12-05 23:08:05 +0800

    wtf!

--- Project/script.js
+++ Project/script.js
@@ -1,3 +1,41 @@
-function operation1(){
-    console.log("Hello world!");
+
+let cartElement = document.querySelector('.cart');
+function toggleCart() {
+    if (cartElement.style.right === '-150%') {
+        cartElement.style.right = '0';
+        container.style.transform = 'translateX(-400px)';
+    } else {
+        cartElement.style.right = '-150%';
+        container.style.transform = 'translateX(0)';
+    }
 }
+
+function operation1(productId) {
+    var x = document.getElementById(productId);
+    if (x.style.display === "none" || x.style.display === "") {
+        x.style.display = "grid";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function decrementQuantity(productId) {
+    var quantityElement = document.querySelector('#' + productId + ' .quantity .value');
+    var currentValue = parseInt(quantityElement.textContent);
+
+    if (currentValue == 0) {
+        operation1(productId);
+    }else{
+        quantityElement.textContent = currentValue - 1;
+    }
+}
+
+function incrementQuantity(productId) {
+    var quantityElement = document.querySelector('#' + productId + ' .quantity .value');
+    var currentValue = parseInt(quantityElement.textContent);
+
+    if (currentValue < 99) {
+        quantityElement.textContent = currentValue + 1;
+    }
+}
+
